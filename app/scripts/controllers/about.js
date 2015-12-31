7dba6d4be5977e76d0682bf802846dcde26431a2
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-12-31 17:57:13 +0900

    first commit with yeoman format

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstAngularApp
+ */
+angular.module('firstAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstAngularApp
+ */
+angular.module('firstAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
